e4b1216f49be8dae45bbdd7d3a000b1a28ff8fc3
whitesoup12 <whitesoup12@gmail.com>
2025-09-15 10:11:35 -0500

    Notification update

--- src/main/java/com/remotefalcon/library/documents/Notification.java
+++ src/main/java/com/remotefalcon/library/documents/Notification.java
@@ -1,8 +1,10 @@
 package com.remotefalcon.library.documents;
 
 import com.remotefalcon.library.enums.NotificationType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,6 +12,8 @@ import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Notification {
     @Id
